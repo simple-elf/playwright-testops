16a5d187a6b72574ad38fd0f64b953d48650f1ef
Anton Aftakhov <aaftahov@rvision.ru>
2025-08-26 19:55:48 +0500

    first autotest

--- src/tests/feature1.test.ts
+++ src/tests/feature1.test.ts
@@ -3,8 +3,8 @@ import * as allure from "allure-js-commons";
 
 test('главная страница содержит заголовок @allure.id:1', async ({ page }) => {
   await allure.owner("kostya");
-  await allure.step("Ручной шаг 1", async () => {});
-  await allure.step("Ручной шаг 2", async () => {});
-  await allure.step("Ручной шаг 3", async () => {});
-  await allure.step("Ручной шаг 4", async () => {});
+  await allure.step("Авто шаг 1", async () => {});
+  await allure.step("Авто шаг 2", async () => {});
+  await allure.step("Авто шаг 3", async () => {});
+  await allure.step("Авто шаг 4", async () => {});
 });
