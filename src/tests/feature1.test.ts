de256cc1a710dcbfe9c2325420a0ea001362a2b6
Anton Aftakhov <aaftahov@rvision.ru>
2025-08-26 22:30:37 +0500

    PR-1

--- src/tests/feature1.test.ts
+++ src/tests/feature1.test.ts
@@ -1,6 +1,12 @@
 import { test, expect } from '@playwright/test';
 import * as allure from "allure-js-commons";
 
+test('Авто сценарий 3 @allure.id:3', async ({ page }) => {
+  await allure.owner("kostya");
+  await allure.step("Авто шаг 1", async () => {});
+  await allure.step("Авто шаг 2", async () => {});
+});
+
 test('Авто сценарий 1 @allure.id:1', async ({ page }) => {
   await allure.owner("kostya");
   await allure.step("Авто шаг 1", async () => {});
