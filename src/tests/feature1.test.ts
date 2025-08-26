a964b7d9cb094a52b94943dc9b4bb0d479dfbcdd
Anton Aftakhov <aaftahov@rvision.ru>
2025-08-26 22:00:23 +0500

    release 2.0 branch

--- src/tests/feature1.test.ts
+++ src/tests/feature1.test.ts
@@ -8,3 +8,10 @@ test('Авто сценарий 1 @allure.id:1', async ({ page }) => {
   await allure.step("Авто шаг 3", async () => {});
   await allure.step("Авто шаг 4", async () => {});
 });
+
+test('Авто сценарий 2 @allure.id:2', async ({ page }) => {
+  await allure.owner("kostya");
+  await allure.step("Авто шаг 1", async () => {});
+  await allure.step("Авто шаг 2", async () => {});
+  await allure.step("Авто шаг 3", async () => {});
+});
